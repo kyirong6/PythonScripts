16b92d8b2a0913e33203f9060bbdcb03a367c8a4
Choenden <kyirong6@gmail.com>
2016-03-12 16:00:04 -0500

    Basic script that takes in Json of Tiwtter data and performs simple Sentiment analysis.

---
+++ Repustate.py
@@ -0,0 +1,29 @@
+import json
+from repustate import Repustate
+
+client = Repustate(api_key='85ef51d423a7fb245eddef56dbbdaacd366629e7', version='v3')
+client.sentiment("This is a great day to go for a run")
+
+
+def script(data):
+    with open(data, 'r') as file:
+        data = json.loads(file)
+    positive = 0
+    negative = 0
+    number_of_tweets = 0
+    for i in data["statuses"]:
+        if sentiment(i["text"]):
+            positive += 1
+            number_of_tweets += 1
+        else:
+            negative += 1
+            number_of_tweets += 1
+    else:
+        pass
+    return float(positive)/number_of_tweets, number_of_tweets
+
+
+
+
+if __name__ == "__main__":
+    script('fun.json')
